0d6746f604b7f13a2d087490c4b7a6ed70b36492
Usuario <Usuario>
2017-01-21 02:44:45 -0600

    Scripts de vida y daño

---
+++ Assets/Scripts/Buildings/BuildingsHealth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingsHealth : MonoBehaviour {
+
+    /// <summary>
+    /// Vida total del edificio
+    /// </summary>
+    private float health = 100;
+
+    public float Health
+    {
+        get
+        {
+            return this.Health;
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Monster monster = other.GetComponent<Monster>();
+
+        if (monster == null)
+        {
+            return;
+        }
+
+        this.health -= monster.Damage * Time.deltaTime;
+    }
+}
