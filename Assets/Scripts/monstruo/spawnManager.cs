d160bfe966ddcade66a9f5923cedd2be50fb24bb
Freihart <carlosfabian512@gmail.com>
2017-01-21 14:14:55 -0600

    validations in spawnmanager and add logic camera to game scen

--- Assets/Scripts/monstruo/spawnManager.cs
+++ Assets/Scripts/monstruo/spawnManager.cs
@@ -4,29 +4,41 @@ using UnityEngine;
 
 public class spawnManager : MonoBehaviour {
 	
-	public GameObject[] monsterTypes;       // An array of the mounter type prefab to be spawned.
 	public float spawnTime = 3f;            // How long between each spawn.
+	public int limitMonsters = 7;			// Maximum of the monsters iin scene
 	public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+	public GameObject[] monsterTypes;       // An array of the mounter type prefab to be spawned.
 
 
 	void Start ()
 	{
 		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+		createMonster();
 		InvokeRepeating ("Spawn", spawnTime, spawnTime);
 	}
 
 
 	/// <summary>
-	/// Create random type of monster in a random spawn point 
+	/// Create random type of monster in a random spawn point whole number of monsters are less than limitMonsters
 	/// </summary>
 	void Spawn ()
 	{
-		//Select the spawn point to create the enamy
-		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-		//Select the mounterType to create the enamy
-		int monsterTypeIndex = Random.Range (0, monsterTypes.Length);
+		if(GameObject.FindGameObjectsWithTag("monster").Length < limitMonsters)
+			createMonster ();
+	}
 
-		// Create an instance of the mounter prefab at the randomly selected spawn point's position and rotation.
-		Instantiate (monsterTypes[monsterTypeIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	bool createMonster(){
+		try{
+			//Select the spawn point to create the enamy
+			int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+			//Select the mounterType to create the enamy
+			int monsterTypeIndex = Random.Range (0, monsterTypes.Length);
+
+			// Create an instance of the mounter prefab at the randomly selected spawn point's position and rotation.
+			Instantiate (monsterTypes[monsterTypeIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+			return true;
+		}
+		catch{ return false; }
 	}
 }
