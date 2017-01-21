49c0f9e0178beb372d81afaceb71bbf2d158f77e
Dio Brando <lordzero0000@gmail.com>
2017-01-21 16:30:12 -0600

    Replacing velocity with relativeVelocity at collision

--- Assets/Scripts/Torres/FallDamage.cs
+++ Assets/Scripts/Torres/FallDamage.cs
@@ -5,25 +5,21 @@ using UnityEngine;
 public class FallDamage : MonoBehaviour {
 
 	// Use this for initialization
-	Rigidbody rb = new Rigidbody();
-	Vector3 v3Velocity = new Vector3();
 
 	void Start () {
-		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		v3Velocity = rb.velocity;	
+	void Update () {	
 	}
 
 	void OnCollisionEnter (Collision col)
     {
         if(col.gameObject.name == "Suelo")
         {
-			if (v3Velocity.y <= -10.0f) {
-				Object.Destroy(this.gameObject);
-			}
+			if (col.relativeVelocity.magnitude > 10) {
+            	Object.Destroy(this.gameObject);
+    		}
         }
     }
 }
