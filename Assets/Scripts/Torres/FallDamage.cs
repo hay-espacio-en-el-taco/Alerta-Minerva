87f5fe198275dd5038d2f6b6c692f0d27094671a
Dio Brando <lordzero0000@gmail.com>
2017-01-21 15:05:36 -0600

    Issue #39: Tower Falling Damage

---
+++ Assets/Scripts/Torres/FallDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody rb = new Rigidbody();
+	Vector3 v3Velocity = new Vector3();
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		v3Velocity = rb.velocity;	
+	}
+
+	void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.name == "Suelo")
+        {
+			if (v3Velocity.y <= -10.0f) {
+				Object.Destroy(this.gameObject);
+			}
+        }
+    }
+}
