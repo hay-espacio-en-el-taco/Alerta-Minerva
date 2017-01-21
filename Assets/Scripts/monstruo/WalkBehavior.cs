a78f620c6d42dcd0188df67cd39e257cb27ac494
Christian Medinilla <Christian Medinilla>
2017-01-21 03:19:04 -0600

    Add monster!!!

---
+++ Assets/Scripts/monstruo/WalkBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkBehavior : MonoBehaviour {
+    public Rigidbody rb;
+    public float velocity = 1.0F;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Time.deltaTime * velocity;
+        var nextPosition = this.transform.position += Vector3.forward * translation;
+        rb.MovePosition(nextPosition);
+    }
+}
