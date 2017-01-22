573e82053b8d680bd7be39130cd02d5f659787c4
Usuario <Usuario>
2017-01-21 19:01:22 -0600

    Added health to monsters & damage variable to waves

---
+++ Assets/Scripts/monstruo/MonsterHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterHealth : MonoBehaviour {
+
+    public const int WAVE_LAYER = 9;
+
+    [SerializeField]
+    private float _health = 100f;
+
+    public float Heatlh
+    {
+        get { return this._health; }
+        private set
+        {
+            this._health = value;
+            if (_health <= 0)
+            {
+                GameObject.Destroy(this.gameObject);
+            }
+        }
+    }
+
+
+	void OnCollisionStay(Collision other) {
+
+        if (other.gameObject.layer == WAVE_LAYER)
+        {
+            WaveDamageI towerDamageI = other.transform.GetComponent<WaveDamageI>();
+            this.Heatlh -= towerDamageI.DamageAmount * Time.deltaTime;
+        }
+         
+	}
+}
